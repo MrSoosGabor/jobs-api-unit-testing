e51e8cd703be576d1b959e03c10c28c3cea23aef
MrSoosGabor <soos.gabor@jedlik.eu>
2024-12-19 19:52:49 +0100

    AWS beállítások javítva

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+mongoose.set('strictQuery', false);
 const connectDatabase = () => {
   mongoose.connect(process.env.DB_LOCAL_URI).then((con) => {
     console.log(`MongoDB Database connected with host: ${con.connection.host}`);
